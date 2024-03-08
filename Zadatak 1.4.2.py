35f35b8f07d03b05c4f6fc0f3ff57ad6ba4c429e
MZorbas299 <157132838+MZorbas299@users.noreply.github.com>
2024-03-08 11:49:55 +0100

    Add files via upload

---
+++ Zadatak 1.4.2.py
@@ -0,0 +1,18 @@
+try:
+    x = float(input("Ocjena: "))
+
+    if x < 0.0 or x > 1.0:
+        print("Ocjena izvan intervala")
+    if x >= 0.9:
+        print("A")
+    elif x >=0.8:
+        print("B")
+    elif x >=0.7:
+        print("C")
+    elif x >=0.6:
+        print("D")
+    else:
+        print("F")
+
+except ValueError:
+    print("Error")
